e53aef21e3af960070802f3316c580293aff3732
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-04-04 10:02:09 +0530

    Update GeminiBody.jsx

--- src/components/GeminiBody.jsx
+++ src/components/GeminiBody.jsx
@@ -37,8 +37,36 @@ const GeminiBody = () => {
               </p>
               <p>How can I help you today?</p>
             </div>
-            //
-            //
+            <div className="grid grid-cols-4 gap-5 p-5">
+              <div className="h-48 p-4 bg-bgSecondaryColor rounded-xl relative cursor-pointer">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <Compass
+                  size={35}
+                  className="p-1 absolute bottom-2 right-2 bg-bgPrimaryColor text-softTextColor rounded-full"
+                />
+              </div>
+              <div className="h-48 p-4 bg-bgSecondaryColor rounded-xl relative cursor-pointer">
+                <p>What’s the reaction to and impact of autonomous vehicles</p>
+                <Lightbulb
+                  size={35}
+                  className="p-1 absolute bottom-2 right-2 bg-bgPrimaryColor text-softTextColor rounded-full"
+                />
+              </div>
+              <div className="h-48 p-4 bg-bgSecondaryColor rounded-xl relative cursor-pointer">
+                <p>Come up with a recipe for an upcoming event</p>
+                <Youtube
+                  size={35}
+                  className="p-1 absolute bottom-2 right-2 bg-bgPrimaryColor text-softTextColor rounded-full"
+                />
+              </div>
+              <div className="h-48 p-4 bg-bgSecondaryColor rounded-xl relative cursor-pointer">
+                <p>Evaluate and rank common camera categories</p>
+                <Code
+                  size={35}
+                  className="p-1 absolute bottom-2 right-2 bg-bgPrimaryColor text-softTextColor rounded-full"
+                />
+              </div>
+    
             </div>
           </>
         ) : (
